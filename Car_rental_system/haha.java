cf8bdf9412f786f9c9bcc7c9c0d831112f58e88f
OtakuCat <48508062+OtakuCat@users.noreply.github.com>
2019-03-13 15:56:37 +0800

    修改
    再GitHub上修改

--- Car_rental_system/haha.java
+++ Car_rental_system/haha.java
@@ -2,7 +2,7 @@ package Car_rental_system;
 
 public abstract class haha {
 	public static void public static void main(String[] args) {
-		System.out.print("hahaha");
+		System.out.print("嘻嘻嘻嘻");
 	}
 
 }
